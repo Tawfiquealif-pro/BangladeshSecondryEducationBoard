bce5680c230272dfda57295f2afe113931732846
Tawfiquealif-pro <143109034+Tawfiquealif-pro@users.noreply.github.com>
2024-04-30 01:16:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var inputValue = document.getElementById('rollNumber');
+
+function getToPage(){
+    document.getElementById('took').innerHTML.a.href ='Education Board Bangladesh.html'
+}
+if(inputValue = undefined){
+    alert('please input a valid roll number')
+}
+
